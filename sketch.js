6bf4beb65b587e234ba7122426d89fe404c72f61
uglyonlytoday <superjang3357@naver.com>
2022-01-25 23:11:14 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+///<reference path="intellisence/p5.global-mode.d.ts" />
+
+const screenWidth = document.documentElement.clientWidth;
+const screenHeight = document.documentElement.clientHeight;
+
+function setup() {
+	createCanvas(screenWidth, screenHeight);
+}
+
+x = 0
+
+function draw() {
+	ellipse(mouseX, mouseY, 100)
+}
+
+function mousePressed() {
+	r = random(255)
+	g = random(255)
+	b = random(255)
+	background(r, g, b)
+	fill(255 - r, 255 - g, 255 - b)
+}
